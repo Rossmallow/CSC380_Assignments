f57cf8ce0ff00c24d0a2e11c33bfd8295aa1bdad
Ross Nelson <rossn430@gmail.com>
2020-01-21 20:56:30 -0600

    Infinite Recursion

--- 1-Eight_Puzzle/node.py
+++ 1-Eight_Puzzle/node.py
@@ -3,58 +3,68 @@
 # node.py
 
 class Node:
-    def __init__ (self, state, parent = None, zeroPos = 0, children = []):
+    def __init__ (self, state, parent = None, zeroPos = -1, children = []):
         self.state = state
         self.parent = parent
-        self.zeroPos = self.findVal(0, state)
-        self.children = self.findChildren(state, zeroPos)
+        if (zeroPos == -1):
+            self.zeroPos = self.findVal(0, state)
+        else:
+            self.zeroPos = zeroPos
+        if (len(children) == 0):
+            self.children = self.findChildren(state, zeroPos)
+        else:
+            self.children = children
 
     # Find the children nodes for each action
-    def getChildren (state, zeroPos):
+    def findChildren (self, state, zeroPos):
             children = []
-            children.append(state.up(state, zeroPos))
-            children.append(state.down(state, zeroPos))
-            children.append(state.left(state, zeroPos))
-            children.append(state.right(state, zeroPos))
+            children.append(self.up(state, zeroPos))
+            children.append(self.down(state, zeroPos))
+            children.append(self.left(state, zeroPos))
+            children.append(self.right(state, zeroPos))
             return children
 
     # Creates and returns a node with an updated state after a move
     # If the move is impossible, return None
-    def up (state, zeroPos):
+    def up (self, state, zeroPos):
         newState = state
-        if (zeroPos != 7 & zeroPos != 8 & zeroPos != 9): # If tile can move up
+        if (zeroPos >= 7): # If tile can move up
+            print("up")
             newState = self.swap(newState, zeroPos, zeroPos - 3)
-            return Node(newState, self, zeroPos)
+            return Node(newState, self, zeroPos - 3)
         else:
             return None
 
     # Creates and returns a node with an updated state after a move
     # If the move is impossible, return None
-    def down (state, zeroPos):
+    def down (self, state, zeroPos):
         newState = state
-        if (zeroPos != 0 & zeroPos != 1 & zeroPos != 2): # If tile can move down
+        if (zeroPos <= 2): # If tile can move down
+            print("down")
             newState = self.swap(newState, zeroPos, zeroPos + 3)
-            return Node(newState, self, zeroPos)
+            return Node(newState, self, zeroPos + 3)
         else:
             return None
 
     # Creates and returns a node with an updated state after a move
     # If the move is impossible, return None
-    def left (state, zeroPos):
+    def left (self, state, zeroPos):
         newState = state
         if (zeroPos != 3 & zeroPos != 6 & zeroPos != 9): # If tile can move left
+            print("left")
             newState = self.swap(newState, zeroPos, zeroPos + 1)
-            return Node(newState, self, zeroPos)
+            return Node(newState, self, zeroPos + 1)
         else:
             return None
 
     # Creates and returns a node with an updated state after a move
     # If the move is impossible, return None
-    def right (state, zeroPos):
+    def right (self, state, zeroPos):
         newState = state
         if (zeroPos != 0 & zeroPos != 4 & zeroPos != 7): # If tile can move right
+            print("right")
             newState = self.swap(newState, zeroPos, zeroPos - 1)
-            return Node(newState, self, zeroPos)
+            return Node(newState, self, zeroPos - 1)
         else:
             return None 
 
@@ -65,10 +75,11 @@ class Node:
                 return i
     
     # Swaps the values of two positions
-    def swap (pos1, pos2, state):
+    def swap (self, state, pos1, pos2):
         value = state[pos1]
         state[pos1] = state[pos2]
         state[pos2] = value
+        print ("Pos1: {0}, Pos2: {1}".format(state[pos1], state[pos2]))
         return state
 
             
